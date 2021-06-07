7774ebabc2801976b3d4a0ede9f47a4a8dce6232
kenta253”kenta253 <tankechiuriho@yahoo.co.jp>
2021-06-07 11:12:41 +0900

    a

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,11 +8,7 @@
   <div class="row">
   <div class="col-4 side ">
     <div class="mt-3 pl-3 border border-primary rounded">
-<<<<<<< HEAD
-        <a href=" {{ route('homes.index') }}" class="fa-5x">
-=======
         <a href=" {{ route('homes') }}" class="fa-5x">
->>>>>>> 445d301a6a68bc95da2db0bb7010051a2258ffff
             <i class="fas fa-fist-raised">
             勉強！
             </i>
