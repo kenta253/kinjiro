14c9d1fbe7d25fa67fac76b7b696bbca26ca2f9a
kenta253”kenta253 <tankechiuriho@yahoo.co.jp>
2021-06-15 21:50:49 +0900

    [update] ユーザー画像更新

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -5,19 +5,24 @@
 @section('content')
   @include('nav')
   <div class="container">
-    <div class="card mt-3">
-    <nav class="navbar navbar-expand navbar-dark peach-gradient">
-      <div class="card-body text-white text-center">
-        <h2 class="h2 m-0">{{ $tag->hashtag }}</h2>
-        <h4 class="text-right text-white">
-          {{ $tag->articles->count() }}件
-        </h4>
-      </div>
-      </nav>
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+      <div class="card mt-3">
+          <nav class="navbar navbar-expand navbar-dark peach-gradient">
+            <div class="card-body text-white text-center">
+              <h2 class="h2 m-0">{{ $tag->hashtag }}</h2>
+              <h4 class="text-right text-white">
+                {{ $tag->articles->count() }}件
+              </h4>
+            </div>
+            </nav>
+          </div>
+          @foreach($tag->articles as $article)
+            @include('articles.card')
+          @endforeach
+        </div>
     </div>
-    @foreach($tag->articles as $article)
-      @include('articles.card')
-    @endforeach
-  </div>
+    </div>
+
 
 @endsection
